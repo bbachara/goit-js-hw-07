516492f7c125e1bb341f244ec0d67d02cb4618b1
Bartek Bachara <bbachara@poczta.onet.pl>
2024-01-25 22:47:55 +0100

    js-h7-homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const picturesContainer = document.querySelector(`.gallery`);
+
+const galleryMarkup = galleryItems.map((image) => {
+  const link = document.createElement(`a`);
+  link.className = `gallery__item`;
+  link.href = image.original;
+
+  const imgElement = document.createElement(`img`);
+  imgElement.className = `gallery__image`;
+  imgElement.src = image.preview;
+  imgElement.alt = image.description;
+  imgElement.dataset.source = image.original;
+
+  link.appendChild(imgElement);
+
+  return link;
+});
+
+picturesContainer.append(...galleryMarkup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionsDataAlt: "Image description",
+});
+gallery.on("show.simplelightbox", function () {
+  console.log("Lightbox is shown");
+});
